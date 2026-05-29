a68dbe2d6f3457b5f5ca80524557f7e50399b22c
irfanasyqy <muhammadirfanalasyqy@gmail.com>
2026-05-29 21:44:20 +0700

    Update API_URL in config.js to the latest endpoint

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 // CUKUP EDIT FILE INI SAAT GANTI URL APPS SCRIPT
 
 const CONFIG = {
-    API_URL: 'https://script.google.com/macros/s/AKfycbzw6fJYRLnsB0GDx4HT7rL4jUKkFGb6ZDliLLH1W8dGwqJxj43xBqV-ZrxiMRv59a7VRA/exec'
+    API_URL: 'https://script.google.com/macros/s/AKfycbzdoXMRfWlviJNBpcyJe0pGJtXkeCbXxs9I6MIFMcLiPAHgJULTgZpodabgzuKPHVb_Ig/exec'
 };
 
 // Jangan ubah kode di bawah ini
